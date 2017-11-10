66bb2df4c7eb39fce45a2e33178f9eea1bb0de76
Katy <ihatetoast@gmail.com>
2017-11-10 15:05:32 -0600

    mostly functional. needs styling and playing to see what is missing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,111 @@
 $(document).ready(function(){
  console.clear();
- 
- gameSetup();
- playGame();
- var rando;
- // on load, start the game
- function gameSetup(){
- 	// instantiate the buttons/meals
-	const cheeseball = new Food("cheeseball", "appetizer");
-	const turkey = new Food("turkey", "protein");
-	const rolls = new Food("rolls", "starch");
-	const greenBeans = new Food("green beans", "veg");
-	// load rando number to reach
-	rando = Math.floor(Math.random() * (102)) + 19;
-	$("#goalNumber").text(rando)
-	cheeseball.serve();
-	cheeseball.load();	
-	turkey.serve();
-	turkey.load();
-	rolls.serve();
-	rolls.load();
-	greenBeans.serve();
-	greenBeans.load();
- 
- }
+ //GET A RANDOM NUMBER FOR PLAYER TO REACH
+ function randoNummo(min, max){
+ 		return Math.floor(Math.random() * (max - min + 1)) + min;
+ 	}
 
-function Food(name, course){
-	this.name = name;
+ 	let losses = 0;
+ 	let wins = 0;
+ 	
+	// ****************************
+	// ****************************
+	// //SET UP THE GAME
+	// ****************************
+	// ****************************
+	function gameSetup(){
+		
+ 	//arrays of ...
+ 		const foodOptions = {
+			appsArr: ["cheeseball", "cranberry sauce", "cauliflower salad"],
+			protArr: ["turkey", "honey-glazed ham", "tofurkey", "turducken"],
+			starchArr: ["mashed potatoes", "stuffing", "taters au gratin", "rolls"],
+			vegArr: ["green beans", "corn on the cob", "peas", "sweet potatoes"]
+ 		}
+	 	//FOR FOOD OPTIONS
+		let appetizer = new Food(foodOptions.appsArr, "appetizer");
+		let protein = new Food(foodOptions.protArr, "protein");
+		let starch = new Food(foodOptions.starchArr, "starch");
+		let veggies = new Food(foodOptions.vegArr, "veg");
+		appetizer.serve();
+		appetizer.load();	
+		protein.serve();
+		protein.load();
+		starch.serve();
+		starch.load();
+		veggies.serve();
+		veggies.load();
+  } //END GAMESETUP FCN
+
+
+	// ****************************
+	// ****************************
+	//			make the food
+	// ****************************
+	// ****************************
+function Food(nameArr, course){
+	// this.nameArr = nameArr;
+	this.index = randoNummo(0, nameArr.length-1)
 	this.course = course;
+	this.name = nameArr[this.index];
 	this.mouthfuls = Math.floor(Math.random()*12) + 1;
 	this.serve = function(){
-		console.log(`I just ate ${this.name} as part of the ${this.course} and it was ${this.mouthfuls} mouthfuls.`);
+		$("#menu").text(`We have ${this.name} for ${this.course} (${this.mouthfuls}) mouthfuls.`);
 	};
 	this.load = function(){
 		$("#foods").append(`<button class=${this.course} id=${this.course} data-key=${this.mouthfuls}>${this.name}</button>`);
 	}
-}
+}//END MAKE THE FOOD FOOD CONSTRUCTOR
 
+
+// ****************************
+// ****************************
+// play the game
+// ****************************
+// ****************************
 //DRY this shit up
 function playGame(){
-	let losses = 0;
- 	let wins = 0;
- 	let userArr = [];
+	let userArr = [];
  	let foodTotal = 0;
+ 	$("#foods").empty();
+ 	$("#mouthful").empty();
+ 	
+ 	let rando = randoNummo(120, 19);
+ //LOAD TO THE STOMACH
+ $("#goalNumber").text(rando);
+
+	gameSetup();
+	
  	$("#mouthful").text(foodTotal);
+ 	$("#goalNumber").text(rando);
  	//get sum of the array
  		function checkWin(){
-		foodTotal = userArr.reduce((sum, value) => sum + value, 0);
-		$("#mouthful").text(foodTotal);
-		if(foodTotal < rando){
-			return;
-		} else {
-			if(foodTotal=== rando){
-				wins ++;
-				$('#wins').text(wins);
+			foodTotal = userArr.reduce((sum, value) => sum + value, 0);
+			$("#mouthful").text(foodTotal);
+			if(foodTotal < rando){
+				return;
 			} else {
-				losses ++;
-				$('#losses').text(losses);
+				if(foodTotal=== rando){
+					wins ++;
+					$('#wins').text(wins);
+					setTimeout(()=>{
+						$("#mouthful").text("Mmm. Piiie.");
+						setTimeout(()=>{
+							playGame();
+						}, 2500);
+					}, 500);
+				} else {
+					losses ++;
+					$('#losses').text(losses);
+					setTimeout(()=>{
+						$("#goalNumber").text("Go home. No pie!");
+						setTimeout(()=>{
+							playGame();
+						}, 2500);
+					}, 500);
+				}
 			}
 		}
-	}
-
 	$("#appetizer").on("click", function(e){
 		const str = $(this).text();
 		const val = $(this).data("key");
@@ -68,7 +113,6 @@ function playGame(){
  		console.log(`value is ${val}`);
  		userArr.push(val);
  		checkWin();
-
  	});
  	$("#protein").on("click", function(e){
 		const str = $(this).text();
@@ -95,16 +139,9 @@ function playGame(){
  		checkWin();
  	});
 
-	console.log(userArr);
 }
 
-
-
-// cheeseball
-// rolls
-// turkey
-// stuffing
-
+playGame();
 
  // 	set up
 	// 4 foots to appear as buttons
